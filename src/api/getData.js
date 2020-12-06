74ffe972ec42a9d57ef452d4c1d042622d944ce1
Jared Loson <jaredloson@gmail.com>
2020-12-06 07:49:51 -0500

    Add data-fetcher

---
+++ src/api/getData.js
@@ -0,0 +1,22 @@
+export const fetchData = async (path, params = {}) => {
+  let url = `https://jsonplaceholder.typicode.com/${path}`;
+
+  const paramsArr = Object.entries(params);
+  paramsArr
+    .filter(([, value]) => typeof value !== "undefined")
+    .forEach(([param, value], idx) => {
+      const paramString = `${param}=${encodeURIComponent(value)}`;
+      url += idx === 0 ? `?${paramString}` : `&${paramString}`;
+    });
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
+
+export default fetchData;
